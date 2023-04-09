e7ae5233a122bb9ef8580cab0df64dc723a5d125
NolimitDougie <102810780+NolimitDougie@users.noreply.github.com>
2023-04-09 18:06:40 -0400

    Update refresh.sh

--- WebHook/refresh.sh
+++ WebHook/refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker pull dtown12/dtown-1:latest
 docker stop docker-website
-docker rm dtown12/dtown-1
+docker rm docker-website
 docker run --name docker-website -d -p 80:80 dtown12/dtown-1:latest
